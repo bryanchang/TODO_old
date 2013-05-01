6da55a0a15273ea918db2cbda0f8dc5bdabf6fc2
Bryan Chang <keunyoung.chang@gmail.com>
2013-05-01 09:26:58 -0700

    Consolidate views into a single view

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TODO::Application.config.secret_token = 'cf9ae8f56453b2542f60071ca145acaa30fdc76a1b2f30d88273a6e22470abb26f2d9613ab799eb1de6d6402d631a7b29eea28f47d8d4ce58479a47c7b1f6be0'
